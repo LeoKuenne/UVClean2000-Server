7d38c0fbae42851db383a896801858e74593c8ed
Leo <leo@k-ku3nn3.de>
2021-02-22 11:31:02 +0100

    Changed menu appearances in list view of uvc groups and devices to a menu bar kind of look

--- src/components/dashboard/UVCGroupList.vue
+++ src/components/dashboard/UVCGroupList.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="p-5 overflow-auto" id="groups">
-    <div class="absolute p-2 items-center bg-white shadow space-y-2">
+    <div class="flex p-2 items-center bg-white shadow">
       <h2 class="text-lg font-bold">Groups</h2>
       <button
         @click="showAddForm"
-        class="w-full text-left text-primary hover:text-gray-600 hover:transform hover:scale-105
-          hover:font-semibold transition-all inline-flex items-center shadow p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5 mx-2" viewBox="0 0 16 16">
+        class="w-full text-left text-primary inline-flex items-center px-2
+        hover:text-gray-600 hover:transform hover:scale-105
+          hover:font-semibold transition-all">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5 mr-2" viewBox="0 0 16 16">
           <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2
             0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
           <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1
